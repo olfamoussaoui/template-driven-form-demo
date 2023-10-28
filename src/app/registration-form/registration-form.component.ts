e9684aa8f24074847b07f78ed2593c33e7eb8387
olfamoussaoui <moussaoui.olfa1@gmail.com>
2023-10-28 18:51:02 +0100

    add validation to the form

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -11,11 +11,23 @@ export class RegistrationFormComponent {
   emailAddress: string;
   jobPosition: String = "";
   pass: string;
-  genderType: string;
+  selectedGender: string;
+  defaultGender = "Male";
+  genderType = [
+    {id: "1", value: "Male"},
+    {id: "2", value: "Female"}
+  ];
   isConfirmed: boolean;
   @ViewChild('registrationForm') form: NgForm;
   onSubmit() {
-    console.log(this.form);
+    this.fullname = this.form.value.name;
+    this.emailAddress = this.form.value.email;
+    this.jobPosition = this.form.value.position;
+    this.pass = this.form.value.password;
+    this.selectedGender = this.form.value.genderTypeDetails.gender;
+    this.isConfirmed = this.form.value.confirmation;
+    console.log(this.form)
+    this.form.reset();
     console.log('Form submitted!!');
   }
 }
